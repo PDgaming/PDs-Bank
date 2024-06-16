82cf3091f4baa760d1715234e1f5847358de3440
PD <89239979+PDgaming@users.noreply.github.com>
2024-06-16 07:24:21 +0530

    Added login and register page files, and added login to navigate to them

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { goto } from "$app/navigation";
+
+    function login() {
+        goto("/login");
+    }
+    function register() {
+        goto("/register");
+    }
+</script>
+
+<center><h1>Welcome! to PD's Bank</h1></center>
+
+<button class="btn" on:click={login}>Login</button>
+<button class="btn" on:click={register}>Register</button>
